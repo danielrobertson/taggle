bc9d374d11dbf9243bdd37cc811cdd6e1ce2b373
Daniel Robertson <danielrobertson733@gmail.com>
2021-12-26 20:21:08 -0600

    feat: multiple editable badged tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,23 @@ import styles from "../styles/Home.module.css";
 import classnames from "classnames";
 
 const Home: NextPage = () => {
-  const [tag, setTag] = useState("japanese");
+  const [tags, setTags] = useState(["japanese"]);
   const [query, setQuery] = useState();
+  const [newTag, setNewTag] = useState("");
+  const [editingTags, setEditingTags] = useState(false);
 
   const handleChange = (event: any) => setQuery(event?.target?.value);
 
-  const handleTagChange = (event: any) => setTag(event?.target?.value);
+  const handleNewTagChange = (event: any) => {
+    if (event.key === "Enter" || event.keyCode === 32) {
+      setTags([...tags, newTag]);
+      setNewTag("");
+      setEditingTags(false);
+      return;
+    }
+
+    setNewTag(event?.target?.value);
+  };
 
   const handleKeyDown = (event: any) => {
     if (event.key === "Enter") {
@@ -20,10 +31,13 @@ const Home: NextPage = () => {
   };
 
   const onSearch = () => {
-    const encodedParams = encodeURIComponent(`${tag} ${query}`);
+    const encodedParams = encodeURIComponent(`${tags.join(" ")} ${query}`);
     const googleUrl = `https://www.google.com/search?q=${encodedParams}`;
     window.location.href = googleUrl;
   };
+
+  const onAddNewTag = () => setEditingTags(true);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -36,15 +50,37 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>Google with tags</h1>
 
         <p className={styles.description}>
-          Current tag:
-          <input
-            className="inline-block placeholder:italic placeholder:text-gray-400 bg-white border border-gray-300 ml-3 py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            placeholder=""
-            type="text"
-            name="search"
-            value={tag}
-            onChange={handleTagChange}
-          />
+          Tags
+          <div className="mt-2">
+            {tags.map((t) => (
+              <span
+                key={t}
+                className="bg-green-700 text-blue-50 py-1 px-2 rounded text-sm font-bold ml-1"
+              >
+                {t}
+              </span>
+            ))}
+            {editingTags && (
+              <input
+                autoFocus
+                className="inline w-1/3 bg-white border border-gray-300 ml-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                placeholder=""
+                type="text"
+                name="tag"
+                value={newTag}
+                onChange={handleNewTagChange}
+                onKeyDown={handleNewTagChange}
+              />
+            )}
+            {!editingTags && (
+              <button
+                className="ml-2 underline text-base text-blue-800"
+                onClick={onAddNewTag}
+              >
+                add
+              </button>
+            )}
+          </div>
         </p>
 
         <label className="relative block min-w-full md:px-16">
