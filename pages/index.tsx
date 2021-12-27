b31fdc290f65fd37667880aed14c4b5d6276c30a
Daniel Robertson <danielrobertson733@gmail.com>
2021-12-26 20:52:04 -0600

    feat: changed tagline

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,9 @@ const Home: NextPage = () => {
 
       <main className={classnames(styles.main, "container mx-auto")}>
         <h1 className={styles.title}>Taggle</h1>
-        <p className="mt-3 text-gray-700 italic">Google with predefined tags</p>
+        <p className="mt-3 text-gray-700 italic">
+          Search Google with predefined tags
+        </p>
 
         <p className={styles.description}>
           <div className="">
